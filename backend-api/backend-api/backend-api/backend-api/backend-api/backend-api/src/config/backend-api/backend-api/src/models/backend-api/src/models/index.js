016d0f20f3930d1ad44da30c0026aed8ab18dabd
AntorSMJN <smjnantor@gmail.com>
2026-08-07 02:48:06 +0600

    Initialize models and associations in index.js

---
+++ backend-api/backend-api/backend-api/backend-api/backend-api/backend-api/src/config/backend-api/backend-api/src/models/backend-api/src/models/index.js
@@ -0,0 +1,82 @@
+const { sequelize } = require("../config/database");
+
+
+const User = require("./User");
+
+const Category = require("./Category");
+
+const Product = require("./Product");
+
+const Order = require("./Order");
+
+const Subscription = require("./Subscription");
+
+
+
+// Category - Product
+
+Category.hasMany(Product, {
+    foreignKey: "categoryId"
+});
+
+
+Product.belongsTo(Category, {
+    foreignKey: "categoryId"
+});
+
+
+
+// User - Order
+
+User.hasMany(Order, {
+    foreignKey: "userId"
+});
+
+
+Order.belongsTo(User, {
+    foreignKey: "userId"
+});
+
+
+
+// User - Subscription
+
+User.hasMany(Subscription, {
+    foreignKey: "userId"
+});
+
+
+Subscription.belongsTo(User, {
+    foreignKey: "userId"
+});
+
+
+
+// Product - Subscription
+
+Product.hasMany(Subscription, {
+    foreignKey: "productId"
+});
+
+
+Subscription.belongsTo(Product, {
+    foreignKey: "productId"
+});
+
+
+
+module.exports = {
+
+    sequelize,
+
+    User,
+
+    Category,
+
+    Product,
+
+    Order,
+
+    Subscription
+
+};
